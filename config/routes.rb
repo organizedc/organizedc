a71289587c718baf5e5c1e88de75a9756215bb35
grafmortifera <grafmortifera@gmail.com>
2017-09-10 09:12:38 +0300

    Генерация контроллера Home#[:index, :about, :contacts]

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'home#index'
+
+  get '/index',     to:  'home#index'
+  get '/about',     to:  'home#about'
+  get '/contacts',  to:  'home#contacts'
+
 end
